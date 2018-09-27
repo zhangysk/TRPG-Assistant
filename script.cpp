c51092221548adeab660220149e738ed5711b413
LonelyFisher <JJHXX0327@hotmail.com>
2018-09-27 18:43:31 +0800

    牛噶老比

--- script.cpp
+++ script.cpp
@@ -5,11 +5,6 @@ namespace ptzs {
 
 Script::Script()
 {
-    maps=new QList<Map*>;
-    BN=new QList<KeyNpc*>;
-    NN=new QList<NormalNpc*>;
-    BS=new QList<BattleScene*>;
-    NS=new QList<NormalScene*>;
 }
 
 
--- script.h
+++ script.h
@@ -17,29 +17,29 @@ public:
     void setName(QString s){name=s;}
     void setSummary(QString s){summary=s;}
     void setAnnouncement(QString s){announcement=s;}
-    void addMap(int index=-1){(index==-1)?maps->append(new Map):maps->insert(index,new Map);}
-    void addBattleNPC(int index=-1){(index==-1)?BN->append(new KeyNpc):BN->insert(index,new KeyNpc);}
-    void addNormalNPC(int index=-1){(index==-1)?NN->append(new NormalNpc):NN->insert(index,new NormalNpc);}
-    void addBattleScene(int index=-1){(index==-1)?BS->append(new BattleScene):BS->insert(index,new BattleScene);}
-    void addNormalScene(int index=-1){(index==-1)?NS->append(new NormalScene):NS->insert(index,new NormalScene);}
+    void addMap(int index=-1){(index==-1)?maps.append(new Map):maps.insert(index,new Map);}
+    void addBattleNPC(int index=-1){(index==-1)?keyNpc.append(new KeyNpc):keyNpc.insert(index,new KeyNpc);}
+    void addNormalNPC(int index=-1){(index==-1)?normalNpc.append(new NormalNpc):normalNpc.insert(index,new NormalNpc);}
+    void addBattleScene(int index=-1){(index==-1)?battleScene.append(new BattleScene):battleScene.insert(index,new BattleScene);}
+    void addNormalScene(int index=-1){(index==-1)?normalScene.append(new NormalScene):normalScene.insert(index,new NormalScene);}
     QString getName(){return name;}
     QString getSummary(){return summary;}
     QString getAnnouncement(){return announcement;}
-    Map* getMap(int index){return maps->at(index);}
-    KeyNpc* getKeyNPC(int index){return BN->at(index);}
-    NormalNpc* getNormalNPC(int index){return NN->at(index);}
-    BattleScene* getBattleScene(int index){return BS->at(index);}
-    NormalScene* getNormalScene(int index){return NS->at(index);}
+    Map* getMap(int index){return maps.at(index);}
+    KeyNpc* getKeyNPC(int index){return keyNpc.at(index);}
+    NormalNpc* getNormalNPC(int index){return normalNpc.at(index);}
+    BattleScene* getBattleScene(int index){return battleScene.at(index);}
+    NormalScene* getNormalScene(int index){return normalScene.at(index);}
 
 private:
     QString name;
     QString summary;
     QString announcement;
-    QList<Map*>* maps;
-    QList<KeyNpc*>* BN;
-    QList<NormalNpc*>* NN;
-    QList<BattleScene*>* BS;
-    QList<NormalScene*>* NS;
+    QList<Map*> maps;
+    QList<KeyNpc*> keyNpc;
+    QList<NormalNpc*> normalNpc;
+    QList<BattleScene*> battleScene;
+    QList<NormalScene*> normalScene;
 };
 
 } // namespace ptzs
