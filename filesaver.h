c5c44aacd614ca1def60f830e453caf216efc9fd
LonelyFisher <JJHXX0327@hotmail.com>
2018-09-27 19:44:29 +0800

    aaaaa

--- filesaver.h
+++ filesaver.h
@@ -1,8 +1,32 @@
 #ifndef FILESAVER_H
 #define FILESAVER_H
 
+#include <QString>
+#include <QFileInfo>
 
 namespace ptzs {
+
+//假设这里接收到的内容是：1.音乐的path	2.文字描述都有具体的描述	3.图片的path
+//我要做的工作：	1.提供播放音乐的函数				ok
+//					2.提供显示图片的函数
+//					3.转存这些文字、图片和音乐，点击时saveAll到指定路径,按照文件夹顺序，并不需要考虑单独存这些情况,并假设已知文件夹路径
+//					4.上述路径都是QSting
+//					5.给文字这些内容标注文件名，并转存到各自文件夹中
+
+/*								volume是1-100之间的值，path是音乐文件的绝对路径。			*/
+/*								这个函数用来播放音乐，加到合适的地方						*/
+void mediaPlay(QString path,int volume){
+    QMediaPlayer *myPlay = new QMediaPlayer;
+    myPlayer->setMedia(QUrl::fromLocalFile(path));
+    myPlay->setVolume(volume);
+    myPlay->play();
+}
+
+void Scropt::saveAll(QString path){
+//					首先创建整体文件夹，保留外部路径
+//					对于文件下每个部分的路径延长字符串，之后进行存储
+
+}
 
 class FileSaver
 {
--- script.cpp
+++ script.cpp
@@ -1,5 +1,13 @@
 #include "script.h"
-
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf_8")
+#endif
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QString>
+#include <QMessageBox>
+#include <QTextStream>
 
 namespace ptzs {
 
@@ -7,6 +15,52 @@ Script::Script()
 {
 }
 
+void Script::fileSaver(QString path){
+    //path是最外层的原始路径
+    //第一步：在该路径下创建一个文件夹并将其命名为name
+    QDir dir;
+    QString folder = path + "/";
+    folder += this->getName();
+    if(!dir.exist(folder)){
+        dir.mkpath(folder);
+    }
+    //在该文件夹下先创建summary，announcement
+    //summary
+    QString fileSumPath = folder + "/summary.txt";
+    QFile fileSummary(fileSumPath);
+    if(!fileSummary.open(QIODevice::WriteOnly | QIODevice::Text)){
+        QMessageBox::warning(this,tr("错误"),tr("打开文件失败"));
+        return;
+    }
+    else{
+        QTextStream textStream(&file);
+        textStream << summary;
+        fileSummary.close();
+    }
+    //announcement
+    QString fileAnnouncePath = folder + "/announcement.txt";
+    QFile fileAnnouncement(fileAnnouncePath);
+    if(!fileAnnouncement.open(QIODevice::WriteOnly | QIODevice::Text)){
+        QMessageBox::warning(this,tr("错误"),tr("打开文件失败"));
+        return;
+    }
+    else{
+        QTextStream textStream(&file);
+        textStream << announcement;
+        fileAnnouncement.close();
+    }
 
+    //创建map文件夹，存储map的一系列图片
+    QString mapFolder = folder + "/map";
+    if(!dir.exist(mapFolder)){
+        dir.mkpath(mapFolder);
+    }
+    //在map文件夹下存储一系列的图片
+
+}
+
+void Script::onAcSaveTriggered(){
+
+}
 
 } // namespace ptzs
